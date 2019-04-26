45552877d427cb46da7d8c68683b7025c8efc7f3
[your <leigh.tm@gmail.com>
2019-04-26 12:05:15 -0400

    woking on the CSS now

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './StarWars.css';
+
+const character = props => {
+    return (
+        <div className= 'character'>
+            <h1><stong>{props.character.name}</stong></h1>
+        
+            <div className='charaterInfo'>
+                <div><strong>Birthyear:</strong> {props.character.birth_year}</div>
+                <div><strong>Gender:</strong> {props.character.birth_gender}</div>
+                <div><strong>Height:</strong> {props.character.height}</div>
+                <div><strong>Mass:</strong> {props.character.mass}</div>
+                <div><strong>Hair Color:</strong> {props.character.hair_color}</div>
+                <div><strong>Skin Color:</strong> {props.character.skin_color}</div>
+                
+                
+            </div>
+        </div>
+    )
+}
+
+export default character;
